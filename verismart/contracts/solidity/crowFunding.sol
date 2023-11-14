dc2d31b60e11239d4288fedc89bbb130d5ae650f
loulankxh <178927469@qq.com>
2023-11-13 20:31:53 -0500

    verismart

--- verismart/contracts/solidity/crowFunding.sol
+++ verismart/contracts/solidity/crowFunding.sol
@@ -1,7 +1,7 @@
-// source: https://github.com/eth-sri/verx-benchmarks/blob/master/overview/main.sol
-
 // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.0;
+pragma solidity ^0.8.0;
+
+// source: https://github.com/eth-sri/verx-benchmarks/blob/master/overview/main.sol
 
 contract Escrow {
   mapping(address => uint256) deposits;
@@ -12,81 +12,112 @@ contract Escrow {
   uint256 totalFunds;
   uint256 raised;
 
+  bool onceWithdraw;
+  bool onceRefund;
+
+  modifier checkInvariant {
+    require(totalFunds == raised || state != State.OPEN);
+    _;
+    assert(totalFunds == raised || state != State.OPEN);
+    require(!(onceRefund && onceWithdraw));
+    _;
+    assert(!(onceRefund && onceWithdraw));
+  }
+
   constructor(address payable b) public {
     beneficiary = b;
     owner = msg.sender;
     totalFunds = 0;
     raised = 0;
+    onceWithdraw=false;
+    onceRefund=false;
   }
 
-  function deposit(address p) onlyOwner public payable {
-    require(totalFunds == raised || state != State.OPEN);
+  function deposit(address p) onlyOwner public checkInvariant payable {
     deposits[p] = deposits[p] + msg.value;
     totalFunds += msg.value;
     raised += msg.value;
-    assert(totalFunds == raised || state != State.OPEN);
   }
 
-  function withdraw() public {
-    require(totalFunds == raised || state != State.OPEN);
+  function withdraw() public checkInvariant {
     require(state == State.SUCCESS);
     beneficiary.transfer(address(this).balance);
     totalFunds = 0;
-    assert(totalFunds == raised || state != State.OPEN);
+    onceWithdraw=true;
   }
 
-  function claimRefund(address payable p) public {
-    require(totalFunds == raised || state != State.OPEN);
+  function claimRefund(address payable p) public checkInvariant {
     require(state == State.REFUND);
     uint256 amount = deposits[p];
     deposits[p] = 0;
     p.transfer(amount);
     totalFunds -= amount;
-    assert(totalFunds == raised || state != State.OPEN);
+    onceRefund=true;
   }
 
   modifier onlyOwner {require(owner == msg.sender); _; }
-  function close() onlyOwner public{state = State.SUCCESS;}
-  function refund() onlyOwner public{state = State.REFUND;}
- // function check() public view {
- //   assert(totalFunds == raised || state != State.OPEN);
- // }
+  function close() onlyOwner public checkInvariant {state = State.SUCCESS;}
+  function refund() onlyOwner public checkInvariant {state = State.REFUND;}
+//  function check() public view {
+//    assert(totalFunds == raised || state != State.OPEN);
+//  }
+//  function checkRefundWithdraw() public view {
+//     assert(!(onceRefund && onceWithdraw));
+//  }
 }
 
-// contract Crowdsale {
-//   Escrow escrow;
-//   uint256 raised = 0;
-//   uint256 goal = 10000 * 10**18;
-//   uint256 closeTime = block.timestamp + 30 days;
-//   bool closed;
-// 
-//   //address payable constant init = payable(address(uint160(0xDEADBEEF)));
-// 
-//   constructor() public{
-//     escrow = new Escrow(address(0xDEADBEEF));
-//     closed = false;
-//   }
-// 
-//   function invest() payable public{
-//     // fix:
-//     require(block.timestamp<=closeTime);
-//     require(raised < goal);
-//     // escrow.deposit{value: msg.value}(msg.sender);
-//     escrow.deposit(msg.sender);
-//     raised += msg.value;
-//   }
-// 
-//   function close() public{
-//     require(block.timestamp > closeTime || raised >= goal);
-//     if (raised >= goal) {
-//       escrow.close();
-//       closed = true;
-//     } else {
-//       escrow.refund();
-// 	 }
-//   }
-// }
-// 
-// contract Deployer{
-//     Crowdsale c = new Crowdsale();
-// }
+contract Crowdsale {
+  Escrow escrow;
+  uint256 raised = 0;
+  uint256 goal = 10000 * 10**18;
+  uint256 closeTime = block.timestamp + 30 days;
+  bool closed;
+  bool onceRefund;
+
+  //address payable constant init = payable(address(uint160(0xDEADBEEF)));
+
+  Tx transaction;
+  enum Tx {
+    Invest, Close
+  }
+
+  modifier checkInvariant {
+    require(!(onceRefund && (raised >= goal)));
+    _;
+    assert(!(onceRefund && (raised >= goal)));
+  }
+
+  constructor() public{
+    escrow = new Escrow(payable(address(0xDEADBEEF)));
+    closed = false;
+	onceRefund=false;
+  }
+
+  function invest() payable public checkInvariant {
+    // fix:
+    require(block.timestamp<=closeTime);
+    require(raised < goal);
+    escrow.deposit{value: msg.value}(msg.sender);
+    raised += msg.value;
+    transaction = Tx.Invest;
+  }
+
+  function close() public checkInvariant {
+    require(block.timestamp > closeTime || raised >= goal);
+    if (raised >= goal) {
+      escrow.close();
+      closed = true;
+    } else {
+      escrow.refund();
+      onceRefund=true;
+	 }
+    transaction = Tx.Close;
+  }
+//  function checkRefundAndRaised() public view {
+//     assert(!(onceRefund && (raised >= goal)));
+//  }
+}
+
+contract Deployer{
+    Crowdsale c = new Crowdsale();
+}
